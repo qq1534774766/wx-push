4ca4e78bed9344097552324ff935a0a07fdf36a8
wenqiaogang <wenqiaogang@sinohealth.cn>
2022-08-23 17:52:01 +0800

    时间修改1

--- src/main/java/com/lx/wxpush/controller/wxController.java
+++ src/main/java/com/lx/wxpush/controller/wxController.java
@@ -40,7 +40,7 @@ public class wxController {
      * 每天早上7：30执行推送
      * @return
      */
-    @Scheduled(cron = "0 30 7 ? * *")
+    @Scheduled(cron = "0 00 8 ? * *")
     @RequestMapping("/send")
     public String send() {
         return sendService.sendWeChatMsg();
